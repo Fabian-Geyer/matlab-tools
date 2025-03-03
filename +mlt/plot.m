70542c103bbd7577349ecabf58403d2477c09a63
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2025-03-03 17:46:14 +0100

    add plot function with custom settings

---
+++ +mlt/plot.m
@@ -0,0 +1,31 @@
+function h = plot(x, y, varargin)
+    % Create a new figure or use the current one
+    if isempty(findobj('Type', 'figure'))
+        h = figure;
+    else
+        h = gcf;
+    end
+
+    % Plot the data
+    plotHandle = plot(x, y, varargin{:});
+
+    % Apply default settings
+    set(plotHandle, 'LineWidth', 1); % Set default line width
+    set(gca, 'TickLabelInterpreter', 'latex'); % Set interpreter for tick labels
+    set(gca, 'FontSize', 12); % Set default font size
+    title('', 'Interpreter', 'latex'); % Set title interpreter
+    xlabel('', 'Interpreter', 'latex'); % Set x-label interpreter
+    ylabel('', 'Interpreter', 'latex'); % Set y-label interpreter
+
+    % Set legend interpreter to LaTeX
+    if ~isempty(legend)
+        legendHandle = legend;
+        set(legendHandle, 'Interpreter', 'latex');
+    end
+
+
+    % Return the plot handle if needed
+    if nargout > 0
+        h = plotHandle;
+    end
+end
