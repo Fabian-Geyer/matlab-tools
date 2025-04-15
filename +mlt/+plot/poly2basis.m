6255e864f5ecc0bd7d30696ddd91af0b4e405bdd
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2025-04-15 17:32:39 +0200

    fix formatting and add missing semicolons in poly2basis function

--- +mlt/+plot/poly2basis.m
+++ +mlt/+plot/poly2basis.m
@@ -6,19 +6,19 @@ function figHandle = poly2basis(p,opts)
     end
 
     % Figure
-    hfigure(opts.figID)
+    hfigure(opts.figID);
 
     % Coefficients
     c = full(poly2basis(p));
 
     % Monomial labels
     monoms = casos.PD(p.monomials.to_vector);
-    labels = arrayfun(@(mon) mon.to_char, monoms, 'UniformOutput',false)
+    labels = arrayfun(@(mon) mon.to_char, monoms, 'UniformOutput',false);
 
     % bar chart
-    barh(c) % horizontal bar chart
-    yticks(1:length(monoms)) % Set y-ticks to match number of monomials
-    yticklabels(labels) % Assign monomial labels to y-axis
-    xlabel('Coefficient Value')
-    title(opts.title)
+    barh(c); % horizontal bar chart
+    yticks(1:length(monoms)); % Set y-ticks to match number of monomials
+    yticklabels(labels); % Assign monomial labels to y-axis
+    xlabel('Coefficient Value');
+    title(opts.title);
 end
